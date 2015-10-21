0f3bea90c9db002a73c37cd8b2ca62fc87ece439
Radek 'blufor' Slavicinsky <radek@blufor.cz>
2015-10-21 21:15:50 +0200

    prepare puppet module

--- puppet/manifests/init.pp
+++ puppet/manifests/init.pp
@@ -1,12 +1,38 @@
-# = Class: portauthority
+# == Class: portauthority
 #
+# Puppet module to bootstrap Port Authority infrastructure.
+#
+# === Parameters
+#
+# [*cluster_enabled*]
+# [*cluster_members*]
+# [*log_destination*]
+# [*host_ip*]
+# [*floating_ip*]
+# [*docker_bridge_ip*]
+# [*dns*]
+# [*etcd_tag*]
+# [*logger_tag*]
+# [*registrator_tag*]
+#
+# === Variables
+#
+# === Examples
+#
+#  class { portauthority: }
+#
+# === Authors
+#
+# Radek 'blufor' Slavicinsky <radek@blufor.cz>
+#
+
 class portauthority (
   $cluster_enabled = false,
   $cluster_members = [],
   $log_destination = "",
   $host_ip = $::ipaddress_eth0,
   $floating_ip,
-  $docker_bridge_ip = "192.168.168.1/24"
+  $docker_bridge_ip = "192.168.168.1/24",
   $dns = ["8.8.8.8", "4.4.4.4"],
   $etcd_tag = "latest",
   $logger_tag = "latest",

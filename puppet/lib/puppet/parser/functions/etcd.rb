b9d607429021b02cc44021252fd22fc67a096735
Radek blufor Slavicinsky <radek@blufor.cz>
2016-01-25 17:03:52 +0100

    work on puppet functions

---
+++ puppet/lib/puppet/parser/functions/etcd.rb
@@ -0,0 +1,22 @@
+require 'etcd-tools'
+
+module Puppet::Parser::Functions
+  newfunction(:etcd_get, :type => :rvalue) do |args|
+    hosts = args[1] || [{ host: lookupvar('fqdn'), port: 2379 }]
+    timeout = args[2] || 5
+    ::Etcd::Client.new(cluster: hosts, read_timeout: timeout).get(args[0])
+  end
+
+  newfunction(:etcd_keys, :type => :rvalue) do |args|
+    hosts = args[1] || [{ host: lookupvar('fqdn'), port: 2379 }]
+    timeout = args[2] || 5
+    ::Etcd::Client.new(cluster: hosts, read_timeout: timeout).keys(args[0])
+  end
+
+  newfunction(:etcd_get_hash, :type => :rvalue) do |args|
+    hosts = args[1] || [{ host: lookupvar('fqdn'), port: 2379 }]
+    timeout = args[2] || 5
+    ::Etcd::Client.new(cluster: hosts, read_timeout: timeout).get_hash(args[0])
+  end
+
+end

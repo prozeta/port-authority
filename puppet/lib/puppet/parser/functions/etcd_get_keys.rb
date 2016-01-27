e804c0ec512512b45bcca073a711df00ab365190
Radek blufor Slavicinsky <radek@blufor.cz>
2016-01-27 16:23:21 +0100

    another attempt to fix autoload

--- puppet/lib/puppet/parser/functions/etcd_get_keys.rb
+++ puppet/lib/puppet/parser/functions/etcd_get_keys.rb
@@ -3,7 +3,7 @@ require 'etcd-tools'
 module Puppet
   module Parser
     module Functions
-      newfunction(:etcd_keys, :type => :rvalue) do |args|
+      newfunction(:etcd_get_keys, :type => :rvalue) do |args|
         hosts = args[1] || [{ host: lookupvar('fqdn'), port: 2379 }]
         timeout = args[2] || 5
         ::Etcd::Client.new(cluster: hosts, read_timeout: timeout).get(args[0]).children.map(&:key)

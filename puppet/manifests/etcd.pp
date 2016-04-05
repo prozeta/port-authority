0491a638759031195dcc4514a189519d5d36f206
Radek blufor Slavicinsky <radek@blufor.cz>
2016-04-05 15:37:26 +0200

    v0.5.20
    - switch etcd from sysvinit to upstart

--- puppet/manifests/etcd.pp
+++ puppet/manifests/etcd.pp
@@ -6,19 +6,30 @@ class portauthority::etcd {
   }
 
   file { '/etc/etcd.conf':
-    owner   => 'root',
-    group   => 'root',
+    owner   => 'etcd',
+    group   => 'etcd',
     mode    => '0640',
     require => Package['etcd'],
     content => template('portauthority/etcd.conf.erb'),
   }
 
   file { '/etc/default/etcd':
-    owner   => 'root',
-    group   => 'root',
+    owner   => 'etcd',
+    group   => 'etcd',
     mode    => '0640',
     require => Package['etcd'],
     content => template('portauthority/etcd.default.erb'),
+  }
+
+  file { '/etc/init/etcd.conf':
+    ensure  => file,
+    mode    => '0644',
+    require => Package['etcd'],
+    source  => 'puppet:///modules/portauthority/etcd.upstart',
+  }
+
+  file { '/etc/init.d/etcd':
+    ensure => absent,
   }
 
   service { 'etcd':
@@ -26,6 +37,6 @@ class portauthority::etcd {
     enable     => true,
     hasstatus  => true,
     hasrestart => true,
-    subscribe  => [ File['/etc/etcd.conf'], File['/etc/default/etcd'] ],
+    subscribe  => [ File['/etc/etcd.conf'], File['/etc/default/etcd'], File['/etc/init/etcd.conf'], File['/etc/init.d/etcd'] ],
   }
 }

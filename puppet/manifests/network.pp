75b7cb63804ad80c1c8c3babcfc418c55742beb4
Radek blufor Slavicinsky <radek@blufor.cz>
2016-04-11 13:38:30 +0200

    v0.5.22
    allow ICC on gw bridge and give it a sane name

--- puppet/manifests/network.pp
+++ puppet/manifests/network.pp
@@ -4,7 +4,7 @@ class portauthority::network {
   if ( $portauthority::cluster_enabled == true ) {
     exec { 'pa_network_gwbridge':
       unless  => 'docker network inspect docker_gwbridge',
-      command => "docker network create -d bridge -o com.docker.network.bridge.enable_icc=false -o com.docker.network.bridge.enable_ip_masquerade=true --subnet=${::portauthority::gwbridge_network} --gateway=${::portauthority::gwbridge_address} docker_gwbridge",
+      command => "docker network create -d bridge -o com.docker.network.bridge.name=br-gw -o com.docker.network.bridge.enable_icc=true -o com.docker.network.bridge.enable_ip_masquerade=true --subnet=${::portauthority::gwbridge_network} --gateway=${::portauthority::gwbridge_address} docker_gwbridge",
       path    => '/usr/bin:/usr/sbin:/bin:/sbin:/usr/local/bin:/usr/local/sbin',
     }
   }

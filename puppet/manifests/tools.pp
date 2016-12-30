4a2dfee13ae628e752c7d2e2e6b6daf5bb012326
Tomas Hejatko <arteal@prozeta.eu>
2016-12-30 06:49:07 +0100

    Install specific version of json gem. docker-api gem does not use version locking, so we need to install right version (which is 1.8.3 for Ruby < 2 on Ubuntu 14.04 LTS) before install portauthoriry gem, which deploys docker-api gem as dependency

--- puppet/manifests/tools.pp
+++ puppet/manifests/tools.pp
@@ -6,6 +6,11 @@ class portauthority::tools {
     ensure => present,
   } ->
 
+  package { 'json':
+    ensure   => '1.8.3',
+    provider => 'gem'
+  } ->
+
   package { 'etcd-tools':
     ensure   => latest,
     provider => 'gem',
